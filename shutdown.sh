86b727ae1a93799316b6391c491adbc7ad49faad
joleye <zl@joleye.com>
2015-05-21 15:56:29 +0800

    update main start shell

--- shutdown.sh
+++ shutdown.sh
@@ -1,1 +1,3 @@
-ps aux|grep -v grep|grep node\ app\.js|awk '{print $2}'|xargs kill -9
+#ps aux|grep -v grep|grep node\ app\.js|awk '{print $2}'|xargs kill -9
+
+ps aux|grep -v grep|grep supervisor\ app\.js|awk '{print $2}'|xargs kill -9
--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-nohup node app.js > nohup.log &
+#nohup node app.js > nohup.log &
+nohup supervisor app.js > nohup.log &
