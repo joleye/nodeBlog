62fcf2dca81dc39669b340615de6ef0e390ec43c
joleye <zl@joleye.com>
2017-07-07 08:46:28 +0800

    完善

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var debug = true;
 
 var config = {
 	  // mongodb 配置
-	  db : 'mongodb://112.74.94.69:10001/node_blog_dev',
+	  db : 'mongodb://#{ip}:10001/node_blog_dev',
 
   	// 程序运行的端口
   	port: 80,
