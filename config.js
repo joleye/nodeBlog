45c3ed0dcdef2e651d4daa006a46dc0bb05c4a76
joleye <zl@joleye.com>
2020-07-16 09:35:52 +0800

    修复版本问题

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
-/**/
-
+/*
+* */
 
 var config = {
     debug: true,
     level: 'info',//日志级别 info debug warn error
     appenders: ['file', 'out'],//日志输出类型
     // mongodb 配置
-    db: 'mongodb://blog:123456@localhost:27017/node_blog_db?authMode=scram-sha1',
+    db: 'mongodb://blog:123456@localhost:27017/node_blog_dev?authMode=scram-sha1',
     page_size: 15,
     // 程序运行的端口
     port: 3000,
     site_name : 'nodBlog'
-}
+};
 
 module.exports = config;
--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 var config = require('../config');
 
-mongoose.connect(config.db, function (err) {
-  if (err) {
-    console.error('connect to %s error: ', config.db, err.message);
-    process.exit(1);
-  }
+mongoose.connect(config.db, {useNewUrlParser: true, useUnifiedTopology: true}, function (err) {
+    if (err) {
+        console.error('connect to %s error: ', config.db, err.message);
+        process.exit(1);
+    }
 });
 
 require('./blog');
